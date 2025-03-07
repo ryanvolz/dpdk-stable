7fbcac532e60eb6197258df15bff2e135da605cf
Ryan Volz <rvolz@mit.edu>
2025-03-07 17:08:58 -0500

    Add debian directory for building .deb package

---
+++ debian/dpdk-for-jetson.doc-base.ex
@@ -0,0 +1,20 @@
+Document: dpdk-for-jetson
+Title: Debian dpdk-for-jetson Manual
+Author: <insert document author here>
+Abstract: This manual describes what dpdk-for-jetson is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/dpdk-for-jetson/dpdk-for-jetson.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/dpdk-for-jetson/dpdk-for-jetson.ps.gz
+
+Format: text
+Files: /usr/share/doc/dpdk-for-jetson/dpdk-for-jetson.text.gz
+
+Format: HTML
+Index: /usr/share/doc/dpdk-for-jetson/html/index.html
+Files: /usr/share/doc/dpdk-for-jetson/html/*.html
